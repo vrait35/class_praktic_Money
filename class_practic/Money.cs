26164adf62acad16e210e22acbc28d12a13ad0cf
vrait35 <vrait35@mail.ru>
2018-09-03 03:06:47 +0600

    hahaha

---
+++ class_practic/Money.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_practic
+{
+    class Money
+    {
+        private int note_face_value;
+        private int count;
+        public Money()
+        {
+            note_face_value = 0;
+            count = 0;
+        }
+        public Money(int note_face_value,int count)
+        {
+            SetNoteFaceValue(note_face_value);
+            SetCount(count);
+        }
+        public void SetNoteFaceValue(int note_face_value)
+        {
+            this.note_face_value = note_face_value;
+        }
+        public void SetCount(int count)
+        {
+            this.count = count;
+        }
+        public int GetNoteFaceValue ()
+        {
+            return note_face_value;
+        }
+        public int GetCount()
+        {
+            return count;
+        }
+        public bool IsEnought(int sum)
+        {
+            return count * note_face_value >= sum;
+        }
+        public int GetCountProduct(int sum)
+        {
+            return count * note_face_value / sum;
+        }
+        public int GetSum()
+        {
+            return count*note_face_value;
+        }
+        static public bool TryParceStrInt(string str, ref int result)
+        {            
+            try
+            {
+                result = int.Parse(str);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ class_practic/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_practic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money=new Money();
+            bool isCorrectInsert = false;
+            int buf = 0;
+            string strbuf;
+
+            while (!isCorrectInsert)
+            {
+                Console.Write("Введите номинал купюры: ");
+                isCorrectInsert = Money.TryParceStrInt(Console.ReadLine(),ref buf);
+            }
+
+            money.SetNoteFaceValue(buf);
+            isCorrectInsert = false;
+
+            while (!isCorrectInsert)
+            {
+                Console.Write("Введите количество купюр: ");
+                isCorrectInsert = Money.TryParceStrInt(Console.ReadLine(),ref buf);
+            }
+            isCorrectInsert = false;
+
+            money.SetCount(buf);
+
+            Console.WriteLine("У вас в кошельке "+money.GetSum()+" тенге ");
+
+            int productSum=0;
+            int productCount=0;
+
+            while (!isCorrectInsert)
+            {
+                Console.Write("Введите  стоимость товара: ");
+                isCorrectInsert = Money.TryParceStrInt(Console.ReadLine(), ref productSum);
+            }
+            isCorrectInsert = false;
+
+            while (!isCorrectInsert)
+            {
+                Console.Write("Введите  количество товара: ");
+                isCorrectInsert = Money.TryParceStrInt(Console.ReadLine(), ref productCount);
+            }
+            isCorrectInsert = false;
+
+
+            if (!money.IsEnought(productCount * productSum))
+            {
+                Console.WriteLine("у вас нехватает денег");
+            }
+            else Console.WriteLine("хватает денег");
+
+            Console.WriteLine("у вас хватит денег на" +money.GetCountProduct(productSum) + " товаров");
+        }
+    }
+}
